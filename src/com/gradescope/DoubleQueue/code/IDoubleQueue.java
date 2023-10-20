88e6fc0f38f4542d5730e53c3e1c8b8e28323339
OliP14 <oliverpasquesi@gmail.com>
2023-10-20 15:46:16 -0400

    Added generic types - IDoubleQueue.java

--- src/com/gradescope/DoubleQueue/code/IDoubleQueue.java
+++ src/com/gradescope/DoubleQueue/code/IDoubleQueue.java
@@ -1,63 +1,68 @@
 /*
- Anthony Melnik
- Oliver Pasquesi
- Ben Simmons
+ * Author: Oliver Pasquesi
+ * Author: Ben Simmons
+ * Author: Anthony Melnik
  */
 
 package com.gradescope.DoubleQueue.code;
 
+
 /**
- * IDoubleQueue represents a queue of Double values.
- * Indexing starts at 0.
- *
- *@initialization Ensures:  Queue is empty and able to insert Double values.
- *
- *@defines: max_queue_size: Z
- *
- *@constraints: 0 <= length() <= max_queue_size
- */
-public interface IDoubleQueue
+* IDoubleQueue represents a queue of generic values.
+* Indexing starts at 0.
+*
+*@initialization Ensures:  Queue is empty and able to insert generic values.
+*
+*@defines: max_queue_size: Z
+*
+*@constraints: 0 <= length() <= max_queue_size
+*/
+public interface IDoubleQueue<T>
 {
 
-    //This function's contracts are in the individual classes
-    public void enqueue(Double val);
 
-    /**dequeueContract
-     *
-     * Removes and returns the value from the front of the queue.
-     *
-     *@return The value at the front of queue.
-     *
-     *@pre Queue is not empty.
-     *
-     *@post The removed element is no longer in queue AND length of queue is decremented by 1.
-     *
-     */
-    public Double dequeue();
+   //This function's contracts are in the individual classes
+   public void enqueue(T val);
 
-    /**lengthContract
-     *
-     * Gives the number of elements currently in queue.
-     *
-     *@return The number of elements in the queue.
-     *
-     *@pre None.
-     *
-     *@post The returned value == size of the queue.
-     *
-     */
-    public int length();
 
-    /**toStringContract
- *
- * Converts the state of the queue into a character string.
- *
- *@return A string representation of the queue.
- *
- *@pre None.
- *
- *@post The returned string is an accurate representation of the queue.
- *
- */
+   /**dequeueContract
+    *
+    * Removes and returns the value from the front of the queue.
+    *
+    *@return The value at the front of queue.
+    *
+    *@pre Queue is not empty.
+    *
+    *@post The removed element is no longer in queue AND length of queue is decremented by 1.
+    *
+    */
+   public T dequeue();
+
+
+   /**lengthContract
+    *
+    * Gives the number of elements currently in queue.
+    *
+    *@return The number of elements in the queue.
+    *
+    *@pre None.
+    *
+    *@post The returned value == size of the queue.
+    *
+    */
+   public int length();
+
+
+   /**toStringContract
+*
+* Converts the state of the queue into a character string.
+*
+*@return A string representation of the queue.
+*
+*@pre None.
+*
+*@post The returned string is an accurate representation of the queue.
+*
+*/
 public String toString();
 }
